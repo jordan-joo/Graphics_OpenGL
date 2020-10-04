f9858c538c50eb49f5cf9396dc5cdec46358f0b8
jordan-joo <wer9957@naver.com>
2020-10-05 00:57:44 +0900

    Initial Commit

---
+++ Graphics_OpenGL/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Graphics
+//
+//  Created by 주은미 on 2020/10/05.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
